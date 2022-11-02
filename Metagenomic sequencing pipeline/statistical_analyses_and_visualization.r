e4072f70217235e619eb24d9cfd6648a1698d343
MonaLiu421 <45484925+MonaLiu421@users.noreply.github.com>
2022-11-02 17:31:31 +0800

    Update and rename statistical analyses and visualization.r to statistical_analyses_and_visualization.r

--- Metagenomic sequencing pipeline/statistical_analyses_and_visualization.r
+++ Metagenomic sequencing pipeline/statistical_analyses_and_visualization.r
@@ -1,4 +1,3 @@
-#这里文件用taxa-input
 otu <- read.delim('clipboard', row.names = 1, sep = '\t', stringsAsFactors = FALSE, check.names = FALSE)
 library(vegan)
 otu<-t(otu)
@@ -6,15 +5,14 @@ otu<-t(otu)
 #observed_species<-as.data.frame(observed_species)
 #Chao1  <- estimateR(otu)[2, ]
 #Chao1<-as.data.frame(Chao1)
-#定义函数
 alpha_diversity <- function(x, tree = NULL) {
   observed_species <- estimateR(x)[1, ]
   Chao1 <- estimateR(x)[2, ]
   ACE <- estimateR(x)[4, ]
   Shannon <- diversity(x, index = 'shannon',base = 2)
-  Simpson <- diversity(x, index = 'simpson')    #注意，这里是Gini-Simpson 指数
+  Simpson <- diversity(x, index = 'simpson')    
   
-  #保留四位小数
+  
   Shannon <- sprintf("%0.2f", Shannon)
   Simpson <- sprintf("%0.2f", Simpson)
   result <- data.frame(observed_species, ACE,Chao1, Shannon, Simpson)
@@ -22,7 +20,7 @@ alpha_diversity <- function(x, tree = NULL) {
 alpha1 <- alpha_diversity (otu)
 write.csv(alpha1,"alpha.csv")
 alpha<-read.csv("alpha.csv",header = T,row.names = 1)
-#差异分析
+
 library(ggpubr)
 library(ggplot2)
 library(ggsignif)
@@ -114,26 +112,26 @@ ggsave("alpha.diversity.pdf",p5,width = 6,height = 8)
 #======beta====
 bray_dis <- vegdist(otu, method = 'bray')
 bray_dis<-as.matrix(bray_dis)
-# 读入实验设计
+
 design = read.csv("group.csv", header=T)
-# 过滤数据并排序
+
 idx = colnames(bray_dis) %in% rownames(design)  
-# 将距离矩阵进行主坐标轴分析
+
 pcoa = cmdscale(bray_dis, k=3, eig=T) # k is dimension, 3 is recommended; eig is eigenvalues
 points = as.data.frame(pcoa$points) # get coordinate string, format to dataframme
 colnames(points) = c("x", "y", "z") 
 eig = pcoa$eig
 points = cbind(points, design[match(rownames(points), design[,1]), ])#将行名和第一列匹配，并得出匹配的位置，并将其提取出来
-# 统计发情与不发情间是否有差异显著
-# 统计按description分组下，组间差异的显著性水平；检验10000次
+
+
 adonis_table = adonis(bray_dis~Group, data=design, permutations = 10000) 
-# 获得pvalue值
+
 adonis_pvalue = adonis_table$aov.tab$`Pr(>F)`[1]
 adonis_R2 = adonis_table$aov.tab$R2
-# 显示组间的pvalue值R2
+
 adonis_pvalue
 adonis_R2
-# 绘制主标准轴的第1，2轴
+
 library(ggstar)
 pdf("bray Fecal PCoA.pdf",width = 6,height = 4)
 p6 = ggplot(points, aes(x=x, y=y, color=Group)) +
